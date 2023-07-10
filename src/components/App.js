e0c94c69f66dc4f80730ec7ac9a43af83f0f2b5b
Nihal00 <nihallsuvarna@gmail.com>
2023-07-10 23:46:39 +0530

    submitting code-Mon Jul 10 2023 23:46:38 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [data, setData] = useState('');
+  // Add event listener for input change
+  function handleDataChange(event) {
+    setData(event.target.value);
+  }
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <p>Enter your name:</p>
+        <input onInput={handleDataChange}/>
+        <div>Hello {data}!</div>
     </div>
   )
 }
