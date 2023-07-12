0095d0e548338c13c6dbac9b121b4f1536d59320
Nihal00 <83398133+Nihal00@users.noreply.github.com>
2023-07-12 06:09:01 +0000

    first

--- src/components/App.js
+++ src/components/App.js
@@ -5,16 +5,12 @@ import './../styles/App.css';
 const App = () => {
   const [data, setData] = useState('');
 
-  function handleDataChange(event) {
-   
-    setData(event.target.value);
-  }
+  
 
   return (
     <div>
-        {/* Do not remove the main div */}
         <p>Enter your name:</p>
-        <input onInput={handleDataChange}/>
+        <input onInput={(e) => setData(e.target.value)}/>
         <p>Hello {data}!</p>
     </div>
   )
